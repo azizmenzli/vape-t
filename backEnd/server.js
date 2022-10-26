e494198fa2653fdb80c6bacd129ab3514d6ac2d4
nounou NOUNOU <brathebow@gmail.com>
2022-10-26 15:18:16 +0100

    db settings 2

--- backEnd/server.js
+++ backEnd/server.js
@@ -2,9 +2,9 @@ const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
-const connectDB = require('./config/db');
+
 const port = process.env.PORT || 5000;
-connectDB();
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
