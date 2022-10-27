c97a3a23fd48c572d558a3232dd6e830f4018e7e
oussama-zemzemi <oussama.zem99@gmail.com>
2022-10-27 12:58:38 +0400

    added

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
 const Product = () => {
-  const cardItem=()=>{
-    return(
-      <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+  const cardItem = () => {
+    return (
+      <div class="card" style={{width: "18rem"}}>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
     )
   }
   return (
@@ -19,7 +19,7 @@ const Product = () => {
         <div className="row">
           <div className="col-12 text-center">
             <h1>Prouducts</h1>
-            <hr/>
+            <hr />
           </div>
         </div>
       </div>
